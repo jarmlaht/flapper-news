401e969480643542e0dad45b83dea374c302b380
Jarmo Lahtinen <jarmlaht@gmail.com>
2016-08-05 12:19:04 +0300

    .gitignore added

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
+var mongoose = require('mongoose');
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var mongoose = require('mongoose');
 var jwt = require('express-jwt');
 
 var Post = mongoose.model('Post');
@@ -10,64 +10,28 @@ var User = mongoose.model('User');
 
 var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
 });
 
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
-    if(err){ return next(err); }
+    if(err){ 
+      return next(err);
+    }
 
     res.json(posts);
   });
 });
 
-router.get('/posts/:post', function(req, res, next) {
-  req.post.populate('comments', function(err, post) {
-    if (err) { return next(err); }
-
-    res.json(post);
-  });
-});
-
 router.post('/posts', auth, function(req, res, next) {
-    var post = new Post(req.body);
-    post.author = req.payload.username;
+  var post = new Post(req.body);
+  post.author = req.payload.username;
 
-    post.save(function(err, post){
-        if(err){ return next(err); }
-        res.json(post);
-    });
-});
-
-router.post('/posts/:post/comments', auth, function(req, res, next) {
-    var comment = new Comment(req.body);
-    comment.post = req.post;
-    comment.author = req.payload.username;
-
-    comment.save(function(err, comment){
-        if(err){ return next(err); }
-
-        req.post.comments.push(comment);
-        req.post.save(function(err, post) {
-        if(err){ return next(err); }
-            res.json(comment);
-        });
-    });
-});
-
-router.put('/posts/:post/upvote', auth, function(req, res, next) {
-  req.post.upvote(function(err, post){
-    if (err) { return next(err); }
-
-    res.json(post);
-  });
-});
-
-router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
-  req.post.upvote(function(err, post){
-    if (err) { return next(err); }
+  post.save(function(err, post){
+    if(err){ return next(err); }
 
     res.json(post);
   });
@@ -78,7 +42,7 @@ router.param('post', function(req, res, next, id) {
 
   query.exec(function (err, post){
     if (err) { return next(err); }
-    if (!post) { return next(new Error('can\'t find post')); }
+    if (!post) { return next(new Error("can't find post")); }
 
     req.post = post;
     return next();
@@ -90,42 +54,101 @@ router.param('comment', function(req, res, next, id) {
 
   query.exec(function (err, comment){
     if (err) { return next(err); }
-    if (!comment) { return next(new Error("can\'t find comment")); }
+    if (!comment) { return next(new Error("can't find comment")); }
 
     req.comment = comment;
     return next();
   });
 });
 
-router.post('/register', function(req, res, next){
-    if(!req.body.username || !req.body.password){
-        return res.status(400).json({message: 'Please fill out all fields'});
-    }
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    res.json(post);
+  });
+});
 
-    var user = new User();
-    user.username = req.body.username;
-    user.setPassword(req.body.password)
-    user.save(function (err){
-        if(err){ return next(err); 
-    }
-    return res.json({token: user.generateJWT()})
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.put('/posts/:post/downvote', auth, function(req, res, next) {
+  req.post.downvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', auth, function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.author = req.payload.username;
+  
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
     });
+  });
+});
+
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
+  req.comment.upvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+router.put('/posts/:post/comments/:comment/downvote', auth, function(req, res, next) {
+  req.comment.downvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
 router.post('/login', function(req, res, next){
-    if(!req.body.username || !req.body.password){
-        return res.status(400).json({message: 'Please fill out all fields'});
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+console.log('calling passport)');
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
     }
-
-    passport.authenticate('local', function(err, user, info){
-        if(err){ return next(err); }
-
-        if(user){
-            return res.json({token: user.generateJWT()});
-        } else {
-            return res.status(401).json(info);
-        }
-    })(req, res, next);
+  })(req, res, next);
 });
 
 module.exports = router;
